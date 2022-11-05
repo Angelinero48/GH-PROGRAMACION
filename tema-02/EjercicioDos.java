565ca65e845dca07c5794739402c3f21bad14049
Angelinero48 <116581341+Angelinero48@users.noreply.github.com>
2022-11-05 12:24:31 +0100

    prueba

--- tema-02/EjercicioDos.java
+++ tema-02/EjercicioDos.java
@@ -1,6 +1,6 @@
 public class EjercicioDos{
 	public static void main (String[]args){
-		//@author Ángel Linero Cano
+		//@author Ángel Linero Cano hola
 		//Crear variable nombre y mostrarla por pantalla.
 		String nombre="Ángel";
 		System.out.println("Mi nombre es " + nombre + ".");
