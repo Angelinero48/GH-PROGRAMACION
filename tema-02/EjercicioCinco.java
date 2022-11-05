559ba0d6f39407c19889bf2f4d44f384080800cb
Angelinero48 <116581341+Angelinero48@users.noreply.github.com>
2022-11-05 12:39:12 +0100

    Ejercicio5mod

--- tema-02/EjercicioCinco.java
+++ tema-02/EjercicioCinco.java
@@ -1,13 +1,13 @@
-public class EjercicioCinco{
-	public static void main (String[]args){
-		//@author Ángel Linero Cano
-		//Conversor de pesetas a euros
+public class EjercicioCinco {
+	public static void main(String[] args) {
+		// @author Ángel Linero Cano
+		// Conversor de pesetas a euros
 		double euros;
 		double pesetas;
 		double conv;
-		euros=166.3860;
-		pesetas=6000;
-		conv=pesetas/euros;
-		System.out.println( pesetas + "pesetas serían " + conv + " euros");
+		euros = 166.3860;
+		pesetas = 6000;
+		conv = pesetas / euros;
+		System.out.printf("%.2f pesetas serían %.2f euros", pesetas, conv);
 	}
 }
