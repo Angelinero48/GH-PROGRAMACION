c185fc57c612f5e0d4d2dcaf00e480cbc41e279f
Angelinero48 <116581341+Angelinero48@users.noreply.github.com>
2022-11-05 13:43:55 +0100

    Ejercicios tema-02

---
+++ main/tema_02/Concatenado.java
@@ -0,0 +1,12 @@
+package tema_02;
+
+public class Concatenado {
+    public static void main (String[] args) {
+        char letra1;
+        char letra2;
+        letra1 = 'a';
+        letra2 = 'b';
+        System.out.println(letra1 + letra2);
+        System.out.println(letra1 + "" + letra2);
+    }
+}
